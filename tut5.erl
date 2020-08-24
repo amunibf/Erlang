76845715c08e202bd10456f749581d08a189f205
amunibf <amunibf@gmail.com>
2020-08-24 09:15:12 +0700

    first commit

---
+++ tut5.erl
@@ -0,0 +1,19 @@
+% this module is file tut5.erl
+-module(tut5).
+-export([format_temps/1]).
+
+format_temps([])->
+    ok;
+format_temps([City|Rest])->
+    print_temp(convert_to_celcius(City)),
+    format_temps(Rest).
+
+convert_to_celcius({Name, {c, Temp}})->
+    {Name, {c, Temp}};
+convert_to_celcius({Name, {f, Temp}})->
+    {Name, {c, (Temp-32)*5/9}}.
+
+print_temp({Name, {c, Temp}})->
+    io:format("~-15w ~w c~n", [Name, Temp]).
+    
+
