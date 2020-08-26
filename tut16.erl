8635e009fb2b84d403e7c020d8b74f43656f36d8
amunibf <amunibf@gmail.com>
2020-08-26 11:25:27 +0700

    another commit

--- tut16.erl
+++ tut16.erl
@@ -1,9 +1,34 @@
--module([tut16]).
+-module(tut16).
 -export([start/0, ping/1, pong/0]).
 
 ping(0)->
 	pong ! finished,
-	io:format("ping finished~n"
+	io:format("ping finished~n",[]);
+
+ping(N)->
+	pong ! {ping, self()},
+	receive
+		pong->
+			io:format("Ping received pong~n",[])
+		end,
+		ping(N-1).
+
+pong()->
+	receive
+		finished->
+			io:format("Pong finished~n",[]);
+		{ping, Ping_PID}->
+			io:format("Pong receive ping~n",[]),
+			Ping_PID ! pong,
+			pong()
+	end.
+
+start()->
+	register(pong, spawn(tut16, pong, [])),
+	spawn(tut16, ping, [3]).
+			
+			
+		
 
 
 
